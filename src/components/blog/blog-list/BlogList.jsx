3ea90086e9a11557ffe0d6ca33634e98aaa5b354
yeexng <118996751+yeexng@users.noreply.github.com>
2023-03-06 16:13:10 +0000

    implemented get method but not sure will it be working

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [blogPosts, setBlogPosts] = useState();
+
+  const getBlogPosts = async () => {
+    try {
+      let res = await fetch("url");
+      if (res.ok) {
+        console.log(res);
+        let blogPosts = await res.json();
+        setBlogPosts(blogPosts);
+      } else {
+        console.log("Having Problem in fetching BlogPosts");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
+
   return (
     <Row>
       {posts.map((post) => (
